fb77628bee692c3886fd755e97d5e58d5cf33af9
Nathan Rugg <nmrugg@gmail.com>
2014-01-20 14:27:47 +0800

    Fixed non-introspective quine (forgot I didn't end up using x)

--- others/non-introspective.js
+++ others/non-introspective.js
@@ -1,12 +1,10 @@
 l=console.log
 q=String.fromCharCode(34)
-x='p()'
 a=[]
 function r(b){a.push(b)}
 function p(s,e){a.forEach(function(c){l(s+c+e)});if(s){l('p('+q+q+','+q+q+')');l('p('+q+'r('+q+'+q,q+'+q+')'+q+')')}}
 r("l=console.log")
 r("q=String.fromCharCode(34)")
-r("x='p()'")
 r("a=[]")
 r("function r(b){a.push(b)}")
 r("function p(s,e){a.forEach(function(c){l(s+c+e)});if(s){l('p('+q+q+','+q+q+')');l('p('+q+'r('+q+'+q,q+'+q+')'+q+')')}}")
