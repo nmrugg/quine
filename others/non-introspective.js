33c4573f3cd1b12d7affc1021c99c1459646dee5
Nathan Rugg <nmrugg@gmail.com>
2014-01-20 14:26:12 +0800

    Made the non-introspective quine shorter.

--- others/non-introspective.js
+++ others/non-introspective.js
@@ -1,12 +1,14 @@
+l=console.log
 q=String.fromCharCode(34)
 x='p()'
 a=[]
 function r(b){a.push(b)}
-function p(s,e){a.forEach(function(c){console.log(s+c+e)});if(s){console.log('p('+q+q+','+q+q+')');console.log('p('+q+'r('+q+'+q,q+'+q+')'+q+')')}}
+function p(s,e){a.forEach(function(c){l(s+c+e)});if(s){l('p('+q+q+','+q+q+')');l('p('+q+'r('+q+'+q,q+'+q+')'+q+')')}}
+r("l=console.log")
 r("q=String.fromCharCode(34)")
 r("x='p()'")
 r("a=[]")
 r("function r(b){a.push(b)}")
-r("function p(s,e){a.forEach(function(c){console.log(s+c+e)});if(s){console.log('p('+q+q+','+q+q+')');console.log('p('+q+'r('+q+'+q,q+'+q+')'+q+')')}}")
+r("function p(s,e){a.forEach(function(c){l(s+c+e)});if(s){l('p('+q+q+','+q+q+')');l('p('+q+'r('+q+'+q,q+'+q+')'+q+')')}}")
 p("","")
 p("r("+q,q+")")
