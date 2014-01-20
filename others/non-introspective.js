7b8542c2932224351d20273fd4f7eae5e6bbfed8
Nathan Rugg <nmrugg@gmail.com>
2014-01-20 14:49:54 +0800

    Use || to shorten the non-introspective quine slightly.

--- others/non-introspective.js
+++ others/non-introspective.js
@@ -3,12 +3,12 @@ l=console.log
 q=String.fromCharCode(34)
 a=[]
 function r(b){a.push(b)}
-function p(s,e){a.forEach(function(c){l(s+c+e)});if(s){l('p(z,z)');l('p('+q+'r('+q+'+q,q+'+q+')'+q+')')}}
+function p(s,e){a.forEach(function(c){l(s+c+e)});if(s)l('p(z,z)')||l('p('+q+'r('+q+'+q,q+'+q+')'+q+')')}
 r("z=''")
 r("l=console.log")
 r("q=String.fromCharCode(34)")
 r("a=[]")
 r("function r(b){a.push(b)}")
-r("function p(s,e){a.forEach(function(c){l(s+c+e)});if(s){l('p(z,z)');l('p('+q+'r('+q+'+q,q+'+q+')'+q+')')}}")
+r("function p(s,e){a.forEach(function(c){l(s+c+e)});if(s)l('p(z,z)')||l('p('+q+'r('+q+'+q,q+'+q+')'+q+')')}")
 p(z,z)
 p("r("+q,q+")")
