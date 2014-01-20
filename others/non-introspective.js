68d89d9409878a29e31f229d658429c50d3bad75
Nathan Rugg <nmrugg@gmail.com>
2014-01-20 14:57:54 +0800

    Got the non-introspective quine under 400 bytes.

--- others/non-introspective.js
+++ others/non-introspective.js
@@ -1,14 +1,12 @@
-z=''
 l=console.log
 q=String.fromCharCode(34)
 a=[]
 function r(b){a.push(b)}
-function p(s,e){a.forEach(function(c){l(s+c+e)});if(s)l('p(z,z)')||l('p('+q+'r('+q+'+q,q+'+q+')'+q+')')}
-r("z=''")
+function p(s,e){a.forEach(function(c){l(s?s+c+e:c)});if(s)l('p()')||l('p('+q+'r('+q+'+q,q+'+q+')'+q+')')}
 r("l=console.log")
 r("q=String.fromCharCode(34)")
 r("a=[]")
 r("function r(b){a.push(b)}")
-r("function p(s,e){a.forEach(function(c){l(s+c+e)});if(s)l('p(z,z)')||l('p('+q+'r('+q+'+q,q+'+q+')'+q+')')}")
-p(z,z)
+r("function p(s,e){a.forEach(function(c){l(s?s+c+e:c)});if(s)l('p()')||l('p('+q+'r('+q+'+q,q+'+q+')'+q+')')}")
+p()
 p("r("+q,q+")")
