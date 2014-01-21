ad5e50abd72caee8c56b590ddd4feae832c83e48
Nathan Rugg <nmrugg@gmail.com>
2014-01-21 12:39:11 +0800

    Shortened the code-data quine by using a lambda.

--- others/fixed-point-code-data.js
+++ others/fixed-point-code-data.js
@@ -1,1 +1,1 @@
-function p(a){q=''+Buffer([34]);console.log(a+'p('+q+a+q+')')};p("function p(a){q=''+Buffer([34]);console.log(a+'p('+q+a+q+')')};")
++function (a){q=''+Buffer([34]);console.log(a+'('+q+a+q+')')}("+function (a){q=''+Buffer([34]);console.log(a+'('+q+a+q+')')}")
