dd251e3a2eefb7e513ead8593f3026115012769d
Nathan Rugg <nmrugg@gmail.com>
2014-01-21 09:36:02 +0800

    Completely rewrote the fixed-point quine and made two versions.

---
+++ others/fixed-point-code-data.js
@@ -0,0 +1,1 @@
+q=String.fromCharCode(34);function p(a){console.log(a+'p('+q+a+q+')')};p("q=String.fromCharCode(34);function p(a){console.log(a+'p('+q+a+q+')')};")
---
+++ others/fixed-point-data-code.js
@@ -0,0 +1,1 @@
+a=";q=String.fromCharCode(34);console.log('a='+q+a+q+a)";q=String.fromCharCode(34);console.log('a='+q+a+q+a)
