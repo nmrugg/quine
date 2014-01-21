7fc16ec35d8b49e982a65f38e7d42e6386f32a0e
Nathan Rugg <nmrugg@gmail.com>
2014-01-21 12:40:50 +0800

    Removed unnecessary white space.

--- others/fixed-point-code-data.js
+++ others/fixed-point-code-data.js
@@ -1,1 +1,1 @@
-+function (a){q=''+Buffer([34]);console.log(a+'('+q+a+q+')')}("+function (a){q=''+Buffer([34]);console.log(a+'('+q+a+q+')')}")
++function(a){q=''+Buffer([34]);console.log(a+'('+q+a+q+')')}("+function(a){q=''+Buffer([34]);console.log(a+'('+q+a+q+')')}")
